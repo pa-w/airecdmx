a199538ddb320e3f29d7854e37a67bc2726cc166
pa-w <paw@paw.mx>
2016-03-22 14:02:16 -0500

    Commit before lunch.. Starting up Ant and the data

---
+++ js/aire.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+	var conf = {
+		data: {},
+		prequantifiers: {},
+		quantifiers: {
+			maps: {}
+			bars: {},
+			lines: {}
+		}
+
+	};
+	var d = new Ant (conf);
+});
