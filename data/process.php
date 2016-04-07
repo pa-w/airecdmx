ad7e836b96004cb04928da655718ff8ed82c2078
pa-w <paw@paw.mx>
2016-04-06 23:56:20 -0400

    Updated data and processing scripts.

--- data/process.php
+++ data/process.php
@@ -1,7 +1,8 @@
 <?php
 $cn = pg_connect ("dbname=airecdmx user=paw");
-$q = pg_query ('SELECT * FROM "all"');
+$q = pg_query ('SELECT * FROM "indice" WHERE extract (year FROM datetime) = \'2016\'') or die (pg_last_error ());
 while ($f = pg_fetch_assoc ($q)) { 
+	echo "..\n";
 	foreach ($f as $k => $v) { 
 		if (!empty ($f ['datetime'])) {
 			@list ($est, $cont) = explode ("_", $k);
@@ -10,6 +11,7 @@ while ($f = pg_fetch_assoc ($q)) {
 			$cont = trim (implode ("_", array_slice ($x, 1)));
 			if (!empty ($cont) && $cont != "fid") { 
 				echo "{$f['datetime']} $est $cont = $v\n";
+				if (!is_numeric ($v)) $v = -1;
 				pg_query ("INSERT INTO aire (fecha,estacion,contaminante,medicion) VALUES ('{$f['datetime']}', '{$est}', '{$cont}', '{$v}')");
 			}
 		}
